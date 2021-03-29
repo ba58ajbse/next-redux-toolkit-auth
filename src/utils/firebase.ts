6a809120e16ca3e7b8636e1bd4aae16b9da7c5ba
shibuya <shibuya@pyxos-jk.co.jp>
2021-03-29 15:32:56 +0900

    add firebasestore

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
+import 'firebase/firestore'
 
 const config = {
   apiKey: process.env.NEXT_PUBLIC_FB_API_KEY,
@@ -14,4 +15,5 @@ if (firebase.apps.length === 0) {
   firebase.initializeApp(config)
 }
 const auth = firebase.auth()
-export { auth }
+const db = firebase.firestore()
+export { auth, db }
