2c5255efa140d562c33936a1d05f00983c7e46a7
shibuya <shibuya@pyxos-jk.co.jp>
2021-03-31 14:49:24 +0900

    エラーが出たため削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,6 @@
 import Dashboard from './dashboard'
-import { useRouter } from 'next/router'
-import { useSelector } from 'react-redux'
-import { selectAuth } from '../store/slices/authSlice'
 
 const IndexPage = () => {
-  const router = useRouter()
-  const userState = useSelector(selectAuth)
-
-  if (!userState.authenticated) {
-    router.push('/login')
-  }
-
   return (
     <>
       <Dashboard />
